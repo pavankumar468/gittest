ec79a158ad1c5f050b7ba71e903a106156fc454b
Pavan Kumar <pavankumarjan8@gmail.com>
2021-03-16 11:48:38 +0530

    here

--- add.c
+++ add.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
-	int a=5,b=6;
-	printf("add = %d",a+b);
+	int a=5,b=9,ch;
+	printf("add or sub");
+	scanf("%d",&ch);
+	if(ch==1){
+		printf("add = %d",a+b);
+	}else{
+		printf("sub = %d",a-b);
+	}
+	
 }
